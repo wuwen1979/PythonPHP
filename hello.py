e1742b0f8f525cb3352ad65c1276cb2f66b02e30
wuwen1979 <wuwen1979@outlook.com>
2017-07-11 14:46:12 +0800

    study how to use function

--- hello.py
+++ hello.py
@@ -1,1 +1,22 @@
-print("hello,world")
+#coding=utf-8
+
+def sayHello():
+    print("你好")
+    print("hello,world")
+sayHello()
+
+
+def sayHi(x):
+    if x=='dog':
+        print("wangwang!")
+    elif x=='cat':
+        print("miao,miao")
+sayHi('dog')
+
+def sayHi():
+    global x
+    print("x is ",x)
+x=50
+sayHi()
+
+
